4484582ef220b8a8013890847ee365c2eca77fe4
Calamari <g.tavonius@gmail.com>
2022-07-18 19:21:07 +0200

    Fix setup script because of wrong bash used

--- setup_my_app.sh
+++ setup_my_app.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/bash
 # This file is copied and adapted from https://github.com/mirego/elixir-boilerplate/blob/master/boilerplate-setup.sh
 
 # -----------------------------------------------------------------------------
